76bdbcc39a397b274a4e3eb38b7a96819a0d0a8a
Anttoni Lahtinen <anttoni.lahtinen@geniem.com>
2018-01-12 15:05:27 +0200

    Run after plugins are loaded

--- plugin.php
+++ plugin.php
@@ -11,10 +11,14 @@
 
 namespace Geniem;
 
-/**
- * Pro version defines this constant so we check if it exists
- * and define it if it doesn't.
- */
-if ( ! defined( 'PLL_LINGOTEK_AD' ) )  {
-    define( 'PLL_LINGOTEK_AD', false );
+add_action( 'plugins_loaded', __NAMESPACE__ . '\\remove_polylang_ads' );
+
+function remove_polylang_ads() {
+	/**
+	 * Pro version defines this constant so we check if it exists
+	 * and define it if it doesn't.
+	 */
+	if ( ! defined( 'PLL_LINGOTEK_AD' ) )  {
+	    define( 'PLL_LINGOTEK_AD', false );
+	}
 }
